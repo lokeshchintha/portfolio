66332559e9ba0d62699f2d4b25c3fe631ae6f094
lokeshchintha <20a91a05e6@aec.edu.in>
2023-08-31 19:48:52 +0530

    portfolio

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import ProjectCard from "./ProjectCards";
+
+import PrivateChat from "../../Assets/Projects/PrivateChat.png";
+import Portfolio from "../../Assets/Projects/Portfolio.png";
+import Ecart from "../../Assets/Projects/ecart.png";
+
+import "./project.css";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="Fluorescent-Blue">Works </strong>
+        </h1>
+        <p>Here are a few projects I've worked on recently.</p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={Ecart}
+              title="Todolist"
+              description="Developed a personalized To-do list website enabling users to create customized daily task lists with user-defined names.Leveraged MongoDB Atlas for seamless database integration, ensuring secure storage of user data on the platform."
+              ghLink="https://github.com/lokeshchintha/Todolist"
+              demoLink="http://todolist.lokeshwebsites.tech/"
+            />
+          </Col>
+
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={Portfolio}
+              title="Portfolio Website"
+              description="My personal Portfolio Website build with React and Bootstrap. It is fully responsive website which supports both dark and light mode."
+              ghLink="https://github.com/rahuljha4171/Portfolio-Website"
+              demoLink="https://rahuljha.tech/"
+            />
+          </Col>
+          <Col md={6} lg={4} className="project-card">
+            <ProjectCard
+              imgPath={PrivateChat}
+              title="Notepad"
+              description="Created a web-based Notepad website that enable users to securely store their notes, utilizing cached data storage for seamless access.Implemented a user-friendly interface with a 'Compose' button in the navigation bar, facilitating easy note creation."
+              ghLink="https://github.com/lokeshchintha/Notepad"
+              demoLink="http://notepad.lokeshwebsites.tech/"
+            />
+          </Col>
+        </Row>
+      </Container>
+      <ScrollToTop />
+    </Container>
+  );
+}
+
+export default Projects;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import "./footer.css";
+
+function Footer() {
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright"></Col>
+        <Col md="4" className="footer-copywright">
+          <span>
+            Designed and Developed by{" "}
+            <i className="primary-header">Lokesh</i>
+          </span>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                style={{ color: `var(--clr-primary)` }}
+                className="github"
+                href="https://github.com/lokeshchintha"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="github"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                style={{ color: `var(--clr-primary)` }}
+                className="email"
+                href="mailto:lokeshchinta369@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="email"
+              >
+                <AiOutlineMail />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                style={{ color: `var(--clr-primary)` }}
+                className="linkedin"
+                href="https://www.linkedin.com/in/lokeshchintha/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="linkedin"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
